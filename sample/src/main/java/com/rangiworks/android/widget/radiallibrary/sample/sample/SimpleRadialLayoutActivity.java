15ee6a002b6452eec7125af5b21050329d601a0d
Sajjad Tabib <sajjad@rangiworks.com>
2014-08-24 20:20:00 -0700

    sample app updates

--- sample/src/main/java/com/rangiworks/android/widget/radiallibrary/sample/sample/SimpleRadialLayoutActivity.java
+++ sample/src/main/java/com/rangiworks/android/widget/radiallibrary/sample/sample/SimpleRadialLayoutActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SampleActivity extends ActionBarActivity {
+public class SimpleRadialLayoutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
